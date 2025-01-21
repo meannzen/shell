e30de88751ac03abdcb1ccbe622f58d195abca4f
meannzen <senmeann@gmail.com>
2025-01-21 14:11:16 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{num::ParseIntError, str::FromStr};
+
+use thiserror::Error;
+
+enum Command {
+    EXIT,
+    ECHO(String),
+}
+
+#[derive(Debug, Error)]
+enum CommandError {
+    #[error("command not found")]
+    NotFound,
+}
+
+impl From<ParseIntError> for CommandError {
+    fn from(_value: ParseIntError) -> Self {
+        Self::NotFound
+    }
+}
+
+impl FromStr for Command {
+    type Err = CommandError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let slilted = s.trim().split_once(" ");
+        match slilted {
+            Some((command, option)) => match command {
+                "exit" => {
+                    let number: i32 = option.parse()?;
+                    if 0 == number {
+                        return Ok(Self::EXIT);
+                    }
+                }
+                "echo" => return Ok(Self::ECHO(option.to_string())),
+                _ => {}
+            },
+            _ => {}
+        }
+
+        Err(CommandError::NotFound)
+    }
+}
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -10,11 +52,18 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+        let command = Command::from_str(&input);
 
-        if input.trim() == "exit 0" {
-            break;
+        match command {
+            Ok(c) => match c {
+                Command::EXIT => break,
+                Command::ECHO(s) => {
+                    println!("{}", s)
+                }
+            },
+            Err(e) => {
+                println!("{}: {}", input.trim(), e)
+            }
         }
-
-        println!("{}: command not found", input.trim());
     }
 }
